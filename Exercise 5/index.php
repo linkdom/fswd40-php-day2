b3429033ab65470c365cc2d5f80eb974faa97d47
DominicWieser <dominic.wieser1995@live.at>
2018-06-19 11:20:42 +0200

    adding exercises 5, 6 finished

--- Exercise 5/index.php
+++ Exercise 5/index.php
@@ -2,9 +2,32 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercise 4</title>
   </head>
   <body>
 
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = ""; 
+	$dbname = "phpday2";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+	   die("Connection failed: " . mysqli_connect_error());
+	}
+	// Create database
+	$sql = "CREATE TABLE employee(
+		employee_ID INT NOT NULL PRIMARY KEY,
+		employeeName VARCHAR(100)
+	)";
+	if (mysqli_query($conn, $sql)) {
+	   echo "Table created successfully \n";
+	} else {
+	   echo "Error creating Table!";
+	}
+	mysqli_close($conn);
+?>
   </body>
 </html>
--- Exercise 6/index.php
+++ Exercise 6/index.php
@@ -2,9 +2,30 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercise 4</title>
   </head>
   <body>
 
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = ""; 
+	$dbname = "phpday2";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+	   die("Connection failed: " . mysqli_connect_error());
+	}
+	// Create database
+	$sql = "Insert into employee(employee_ID, employeeName) VALUES
+	(1, 'Kathrin Renz')";
+	if (mysqli_query($conn, $sql)) {
+	   echo "Data inserted successfully \n";
+	} else {
+	   echo "Error Inserting Data";
+	}
+	mysqli_close($conn);
+?>
   </body>
 </html>
