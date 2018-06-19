bb04ac76ea68b30acd74b04bc8fefe3615c771b7
Dominik Link <Vorbild94@gmail.com>
2018-06-19 10:03:59 +0200

    finished exercise 1

--- Exercise 1/index.php
+++ Exercise 1/index.php
@@ -3,8 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="/css/chrome.css">
+    <link rel="stylesheet" href="/css/mozilla.css">
   </head>
   <body>
-
+    <?php
+    $viewer = getenv( "HTTP_USER_AGENT" );
+    $browser = "An unidentified browser";
+    if( preg_match( "/Chrome/i", "$viewer" ) )
+    {
+    $browser = 'chrome';
+    }
+    else if( preg_match( "/Mozilla/i", "$viewer" ))
+    {
+    $browser = "mozilla";
+    }
+    elseif(preg_match('/Safari/i',"$viewer"))
+    {
+    $browser = 'Apple Safari';
+    }
+    echo "<link rel='stylesheet' href='css/" . $browser .".css'>";
+    echo("</br>You are using $browser. </br>");
+    ?>
   </body>
 </html>
