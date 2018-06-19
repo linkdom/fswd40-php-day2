9eda4080f16e496bc6282b72b8387855447c58a3
DominicWieser <dominic.wieser1995@live.at>
2018-06-19 11:11:14 +0200

    adding finished exercise 4

--- Exercise 4/index.php
+++ Exercise 4/index.php
@@ -2,9 +2,29 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercise 4</title>
   </head>
   <body>
 
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = ""; 
+	$dbname = "phpday2";
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+	// Check connection
+	if (!$conn) {
+	   die("Connection failed: " . mysqli_connect_error());
+	}
+	// Create database
+	$sql = "CREATE DATABASE $dbname";
+	if (mysqli_query($conn, $sql)) {
+	   echo "Database $dbname created successfully! \n";
+	} else {
+	   echo "Error creating database $dbname: " . mysqli_error($conn);
+	}
+	mysqli_close($conn);
+?>
   </body>
 </html>
