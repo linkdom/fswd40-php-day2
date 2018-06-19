0e2a797db61ed299cf7314d3d71798d08791ea45
Dominik Link <Vorbild94@gmail.com>
2018-06-19 10:27:28 +0200

    finished exercise 3

--- Exercise 3/index.php
+++ Exercise 3/index.php
@@ -2,9 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Exercise 3</title>
   </head>
   <body>
-
+    <?php
+    function calc ($num1, $num2){
+      $sum = $num1 + $num2;
+      echo "Sum of the two numbers is : $sum";
+    }
+calc(7,22)
+     ?>
   </body>
 </html>
